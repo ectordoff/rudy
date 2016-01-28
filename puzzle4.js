ef41786bcf865286febf5744d47cfd6e08e728ec
ectordoff <elspeth@cca.edu>
2016-01-28 14:10:05 -0800

    Create puzzle4.js

---
+++ puzzle4.js
@@ -0,0 +1,17 @@
+down();
+down();
+down();
+down();
+right();
+var c = getColor();
+down();
+setColor(c);
+right();
+right();
+up();
+right();
+c = getColor();
+down();
+setColor(c);
+right();
+right();
