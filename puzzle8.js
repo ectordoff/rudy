d5d7ef0d27adfbe3be8ada035e6ea216160b7604
ectordoff <elspeth@cca.edu>
2016-02-01 08:55:01 -0800

    Update puzzle8.js

--- puzzle8.js
+++ puzzle8.js
@@ -44,3 +44,46 @@ right();
 repeatDown(7);
 
 right();
+
+OR
+
+function repeatDown(total) {
+  var count =0;
+  while (count < total) {
+    down();
+    count = count + 1;
+  }
+}
+
+repeatDown(8);
+
+right();
+right();
+
+function repeatUp(total) {
+  var count =0;
+  while (count < total) {
+    up();
+    count = count + 1;
+  }
+}
+repeatUp(7);
+
+right();
+right();
+
+
+repeatDown(7);
+
+right();
+right();
+
+repeatUp(7);
+
+right();
+right();
+
+repeatDown(7);
+
+right();
+
