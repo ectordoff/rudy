489825f95371ef986189ecf9f1ea435b9eb16af0
ectordoff <elspeth@cca.edu>
2016-02-01 08:33:01 -0800

    Update puzzle6.js

--- puzzle6.js
+++ puzzle6.js
@@ -10,3 +10,30 @@ if (getColor() == "blue") {
 } else {
   left();}
 down();
+
+
+ACTUALLY
+
+right();
+if (getColor() == "blue") {
+    down();
+  	down();
+  	down();
+  		if (getColor() == "blue") {
+    	right();
+  		down();
+	} else {
+ 	 		left();
+			down();}
+  
+} else {
+  up();
+	up();
+	up();
+	if (getColor() == "blue") {
+    	right();
+  		up();
+	} else {
+ 		left();
+		up();}
+}
